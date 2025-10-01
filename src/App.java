f044c7b662c2d40cf295b6ff80171e4d6c59b971
Eliel Jesus G. V. <elieljgarciavela@gmail.com>
2025-10-01 14:19:14 +0200

    fix: fixed the output and calculations

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         double pesetas = 166.386;
-        double valordeeuros = 0.000080;
+        double valordeeuros = 0.006;
 
-        System.out.printf(pesetas + " pesetas son " + (pesetas * valordeeuros) + " euros.");
+        System.out.printf("%.2f pesetas son %.2f euros",(float)pesetas, (pesetas * valordeeuros) );
     }
 }
